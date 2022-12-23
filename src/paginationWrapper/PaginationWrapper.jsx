1eabbb766f286711994b17c581b37e4cfd4c5bfd
Marcus van Ginkel <mavangin@gmail.com>
2022-12-22 18:26:18 -0600

    add pagination component

--- src/paginationWrapper/PaginationWrapper.jsx
+++ src/paginationWrapper/PaginationWrapper.jsx
@@ -1,5 +1,25 @@
-function PaginationWrapper(){
+import Box from '@mui/material/Box';
+import Pagination from '@mui/material/Pagination';
+import React from 'react';
 
+function PaginationWrapper({ setPage, page, totalItems}) {
+
+	const numberOfPages = Math.ceil(totalItems / 25)
+
+	function handlePageChange(event, value) {
+		setPage(value);
+	}
+
+	return (
+		<Box display="flex" justifyContent="center">
+			<Pagination
+				sx={{ marginBottom: '5rem', marginTop: '5rem' }}
+				count={numberOfPages}
+				page={page}
+				onChange={handlePageChange}
+			/>
+		</Box>
+	);
 }
 
-export default PaginationWrapper
+export default PaginationWrapper;
