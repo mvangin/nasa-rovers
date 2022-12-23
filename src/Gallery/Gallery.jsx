44558acdd67dedc450983c6a4b16fd9545428739
Marcus van Ginkel <mavangin@gmail.com>
2022-12-22 18:27:21 -0600

    add additional image filters

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -1,25 +1,47 @@
+import { cameraMapping } from '../config/rover-cameras';
 import { formatDate } from '../utils/formatDate';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
-import classes from './gallery.module.css';
-import getPhotosByRover from '../Services/GalleryService';
-import Pagination from '@mui/material/Pagination';
-import React, { useState, useEffect } from 'react';
+import FormControl from '@mui/material/FormControl';
+import {
+	getPhotosByRover,
+	getTotalPicturesByRover,
+} from '../Services/GalleryService';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import PaginationWrapper from '../paginationWrapper/PaginationWrapper';
+import React, { useEffect, useState } from 'react';
 import RoverImageGrid from '../Shared/RoverImageGrid';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 function Gallery() {
 	const [photoList, setPhotoList] = useState([]);
-	const [rover, setRover] = useState(null);
-	const [dateObject, setDateObject] = useState(null);
-	const [isFetchingData, setIsFetchingData] = useState(false);
+	const [rover, setRover] = useState('curiosity');
+	const [dateObject, setDateObject] = useState({
+		dateType: 'earth_date',
+		dateString: formatDate(new Date()),
+	});
+	const [isFetchingData, setIsFetchingData] = useState(true);
 	const [page, setPage] = useState(1);
+	const [totalItems, setTotalItems] = useState(1);
+	const [cameraList, setCameraList] = useState(cameraMapping);
+	const [cameraName, setCameraName] = useState('all');
 
-	async function getAndSetPhotosByRover(rover, date, page) {
+	async function getAndSetPhotosByRover(rover, date, cameraName, page) {
 		setIsFetchingData(true);
 		try {
-			const data = await getPhotosByRover(rover, date, page);
-			setPhotoList(data);
+			const photos = await getPhotosByRover(
+				rover,
+				date,
+				cameraName,
+				page
+			);
+			const totalPhotos = await getTotalPicturesByRover(rover, date);
+			setPhotoList(photos);
+			setTotalItems(totalPhotos);
 		} catch (error) {
 			console.log(error);
 		}
@@ -27,52 +49,121 @@ function Gallery() {
 	}
 
 	useEffect(() => {
-		const dateString = formatDate(new Date('2015-06-03'));
+		if (!rover || !dateObject || !page) {
+			return;
+		}
 
-		let dateObject = {
-			dateType: 'earth_date',
-			dateString: dateString,
-		};
+		getAndSetPhotosByRover(rover, dateObject, cameraName, page);
+	}, [page, dateObject, rover, cameraName]);
 
-		setDateObject(dateObject);
-		setRover('curiosity');
-
-		getAndSetPhotosByRover('curiosity', dateObject);
-	}, []);
-
-	function handleSelectRover(selectedRover) {
-		setRover(selectedRover);
+	function handleSelectRover(e) {
+		setRover(e.target.value);
 		setPage(1);
-		getAndSetPhotosByRover(selectedRover, dateObject, 1);
 	}
 
-	function handlePageChange(event, pageValue) {
-		setPage(pageValue);
-		console.log(pageValue, 'va');
-		getAndSetPhotosByRover(rover, dateObject, pageValue);
+	function handleDateChange(event) {
+		setDateObject((prev) => ({ ...prev, dateString: event.target.value }));
 	}
 
-	console.log(photoList);
+	const handleToggleChange = (event) => {
+		setDateObject({
+			dateString:
+				event.target.value === 'sol' ? 0 : formatDate(new Date()),
+			dateType: event.target.value,
+		});
+	};
 
 	return (
 		<div>
-			<div style={{ marginTop: '2rem', textAlign: 'center' }}>
-				Select Rover
-			</div>
-
-			<Box display="flex" justifyContent="center">
+			<Box
+				display="flex"
+				justifyContent="space-around"
+				alignItems="center"
+				marginTop="1rem"
+				marginBottom="1rem"
+				sx={{ flexDirection: { xs: 'column', md: 'row' } }}
+			>
 				<div>
-					<Button onClick={() => handleSelectRover('curiosity')}>
-						Curiosity
-					</Button>
-					<Button onClick={() => handleSelectRover('opportunity')}>
-						Opportunity
-					</Button>
-					<Button onClick={() => handleSelectRover('spirit')}>
-						Spirit
-					</Button>
+					<div>
+						<ToggleButtonGroup
+							color="primary"
+							value={dateObject.dateType}
+							exclusive
+							onChange={handleToggleChange}
+							aria-label="Platform"
+							sx={{ marginBottom: '.5rem' }}
+						>
+							<ToggleButton value="sol">Martian Sol</ToggleButton>
+							<ToggleButton value="earth_date">Date</ToggleButton>
+						</ToggleButtonGroup>
+					</div>
+
+					{dateObject.dateType === 'earth_date' && (
+						<TextField
+							id="outlined-basic"
+							label="Enter date"
+							variant="outlined"
+							type="date"
+							value={dateObject.dateString}
+							onChange={handleDateChange}
+						/>
+					)}
+
+					{dateObject.dateType === 'sol' && (
+						<TextField
+							value={dateObject.dateString}
+							onChange={handleDateChange}
+							id="standard-basic"
+							label="Enter sol day"
+							variant="standard"
+						/>
+					)}
 				</div>
+
+				<FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+					<InputLabel id="rover-label">Select Rover</InputLabel>
+					<Select
+						labelId="rover-label"
+						id="rover-select"
+						value={rover}
+						onChange={handleSelectRover}
+						label="Rover"
+					>
+						<MenuItem value={'curiosity'}>Curiosity</MenuItem>
+						<MenuItem value={'opportunity'}>Opportunity</MenuItem>
+						<MenuItem value={'spirit'}>Spirit</MenuItem>
+					</Select>
+				</FormControl>
+
+				<Box>
+					<FormControl
+						variant="standard"
+						sx={{ m: 1, minWidth: 250 }}
+					>
+						<InputLabel id="rover-camera-label">
+							Select Rover Cameras
+						</InputLabel>
+						<Select
+							labelId="rover-camera-label"
+							value={cameraName}
+							onChange={(event) => {
+								setCameraName(event.target.value);
+							}}
+						>
+							<MenuItem value={'all'}> All Cameras </MenuItem>
+							{cameraList.map((option) => (
+								<MenuItem
+									key={option.value}
+									value={option.value}
+								>
+									{option.label}
+								</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</Box>
 			</Box>
+
 			{isFetchingData ? (
 				<Box display="flex" justifyContent="center" mt={5}>
 					<CircularProgress />
@@ -80,14 +171,11 @@ function Gallery() {
 			) : (
 				<div>
 					<RoverImageGrid photoList={photoList} />
-					<Box display="flex" justifyContent="center">
-						<Pagination
-							sx={{ marginBottom: '5rem', marginTop: '5rem' }}
-							count={10}
-							page={page}
-							onChange={handlePageChange}
-						/>
-					</Box>
+					<PaginationWrapper
+						setPage={setPage}
+						page={page}
+						totalItems={totalItems}
+					/>
 				</div>
 			)}
 		</div>
--- src/Shared/RoverImageGrid.jsx
+++ src/Shared/RoverImageGrid.jsx
@@ -1,40 +1,41 @@
 import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function RoverImageGrid({ photoList }) {
 	function renderPhotoList(photoList) {
 		if (!photoList.length) {
 			return (
-				<Box sx={{ 
-					display: 'flex', 
-					justifyContent: 'center', 
-					marginTop: "5rem",
-					fontSize: "1.5rem;"
-					}}>
+				<Box
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+						marginTop: '5rem',
+						fontSize: '1.5rem;',
+					}}
+				>
 					No photos found.
 				</Box>
 			);
 		}
 
 		return (
-			<div> 
-			<ImageList variant="masonry" cols={3} gap={8}>
-				{photoList.map((photo) => {
-					return (
-						<ImageListItem key={photo.id}>
-							<img
-								alt="rover camera shot"
-								width={'100%'}
-								src={photo.img_src}
-								loading="lazy"
-							/>
-						</ImageListItem>
-					);
-				})}
-				
-			</ImageList>
+			<div>
+				<ImageList variant="masonry" cols={3} gap={8}>
+					{photoList.map((photo) => {
+						return (
+							<ImageListItem key={photo.id}>
+								<img
+									alt="rover camera shot"
+									width={'100%'}
+									src={photo.img_src}
+									loading="lazy"
+								/>
+							</ImageListItem>
+						);
+					})}
+				</ImageList>
 			</div>
 		);
 	}
