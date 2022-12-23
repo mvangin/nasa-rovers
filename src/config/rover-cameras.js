4c05f1599258968977451a19b9848c9353269274
Marcus van Ginkel <mavangin@gmail.com>
2022-12-22 18:25:38 -0600

    add camera mappings js file

---
+++ src/config/rover-cameras.js
@@ -0,0 +1,11 @@
+export const cameraMapping = [
+	{ value : "FHAZ", label : ' Front Hazard Avoidance Camera' },
+	{ value : "RHAZ", label : 'Rear Hazard Avoidance Camera' },
+	{ value : "MAST", label : 'Mast Camera' },
+	{ value : "CHEMCAM", label : 'Chemistry and Camera Complex' },
+	{ value : "MAHLI", label : 'Mars Hand Lens Imager' },
+	{ value : "MARDI", label : 'Mars Descent Imager' },
+	{ value : "NAVCAM", label : 'Navigation Camera' },
+	{ value : "PANCAM", label : 'Panoramic Camera' },
+	{ value : "MINITES", label : 'Mini-TES' },
+];
