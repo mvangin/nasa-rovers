6196cb49de866a3858421e2dcd35b8bba4ade34a
Marcus van Ginkel <mavangin@gmail.com>
2022-12-22 18:26:58 -0600

    add gallery service getTotalPictures and increase filter options

--- src/Services/GalleryService.js
+++ src/Services/GalleryService.js
@@ -1,9 +1,38 @@
-export default async function getPhotosByRover(rover, date, page = 1) {
-	let baseUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers';
+export async function getPhotosByRover(
+	rover,
+	date,
+	camera = 'all',
+	page = 1
+) {
+	let baseUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos`;
 
-	let data = await fetch(
-		`${baseUrl}/${rover}/photos?page=${page}&${date.dateType}=${date.dateString}&api_key=${process.env.REACT_APP_NASA_API_KEY ?? 'DEMO_KEY'}`
-	);
-	data = await data.json();
-	return data.photos;
+	let apiKeyString = `api_key=${
+		process.env.REACT_APP_NASA_API_KEY ?? 'DEMO_KEY'
+	}`;
+
+	let cameraFilter = camera === 'all' ? '' : `&camera=${camera}`;
+
+	let query = `${date.dateType}=${date.dateString}&page=${page}${cameraFilter}`;
+
+	let photoData = await fetch(`${baseUrl}?${query}&${apiKeyString}`);
+
+	photoData = await photoData.json();
+
+	return photoData?.photos;
+}
+
+export async function getTotalPicturesByRover(rover, date) {
+	let baseUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos`;
+
+	let apiKeyString = `api_key=${
+		process.env.REACT_APP_NASA_API_KEY ?? 'DEMO_KEY'
+	}`;
+
+	let query = `${date.dateType}=${date.dateString}`;
+
+	let totalPictures = await fetch(`${baseUrl}?${query}&${apiKeyString}`);
+
+	totalPictures = await totalPictures.json();
+
+	return totalPictures?.photos?.length;
 }
