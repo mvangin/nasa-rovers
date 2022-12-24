bb1cf696736afdda32596273cbe2733df8f68abe
Marcus van Ginkel <mavangin@gmail.com>
2022-12-23 22:50:21 -0600

    cleanup RoverImagesGrid

--- src/Shared/RoverImageGrid.jsx
+++ src/Shared/RoverImageGrid.jsx
@@ -4,43 +4,39 @@ import ImageListItem from '@mui/material/ImageListItem';
 import React from 'react';
 
 function RoverImageGrid({ photoList }) {
-	function renderPhotoList(photoList) {
-		if (!photoList.length) {
-			return (
-				<Box
-					sx={{
-						display: 'flex',
-						justifyContent: 'center',
-						marginTop: '5rem',
-						fontSize: '1.5rem;',
-					}}
-				>
-					No photos found.
-				</Box>
-			);
-		}
-
+	if (!photoList?.length) {
 		return (
-			<div>
-				<ImageList variant="masonry" cols={3} gap={8}>
-					{photoList.map((photo) => {
-						return (
-							<ImageListItem key={photo.id}>
-								<img
-									alt={`rover camera shot ${photo.id}`}
-									width={'100%'}
-									src={photo.img_src}
-									loading="lazy"
-								/>
-							</ImageListItem>
-						);
-					})}
-				</ImageList>
-			</div>
+			<Box
+				sx={{
+					display: 'flex',
+					justifyContent: 'center',
+					marginTop: '5rem',
+					fontSize: '1.5rem;',
+				}}
+			>
+				No photos found.
+			</Box>
 		);
 	}
 
-	return <div>{photoList && renderPhotoList(photoList)}</div>;
+	return (
+		<div>
+			<ImageList variant="masonry" cols={3} gap={8}>
+				{photoList.map((photo) => {
+					return (
+						<ImageListItem key={photo.id}>
+							<img
+								alt={`rover camera shot ${photo.id}`}
+								width={'100%'}
+								src={photo.img_src}
+								loading="lazy"
+							/>
+						</ImageListItem>
+					);
+				})}
+			</ImageList>
+		</div>
+	);
 }
 
 export default RoverImageGrid;
